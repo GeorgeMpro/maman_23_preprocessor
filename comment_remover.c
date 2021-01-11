b0d5f2e9515854b35be08137b10067844956d30e
george <George.mpro87@gmail.com>
2021-01-11 19:05:57 +0200

    Refactored,starting phase 2

--- comment_remover.c
+++ comment_remover.c
@@ -4,74 +4,73 @@ enum state {
     IN_COMMENT, OUT, IN_STRING, LEFT_SLASH, RIGHT_STAR
 };
 
-FILE *write_file_without_comments(FILE *in, FILE *out);
 
-/*todo move to reader/writer*/
-FILE * generate_comment_free_file(char *fileName) {
-
+/*Generate a copy of given file without ANSI comments*/
+FILE *generate_comment_free_file(char *fileName) {
     FILE *in = NULL, *out = NULL;
 
     /*Get pointer to file is possible*/
     in = getFile(fileName, READ_MODE);
-    out = createOutFile(fileName, FILE_NO_COMMENT, "w");
-
-/*todo uncomment
- * switch cases? in, out, comment in comment?*/
-    /*Reading and writing to file*/
-
-    /*todo clear memory, also file pointers*/
+    out = createOutFile(fileName, FILE_NO_COMMENT, WRITE_MODE);
 
     return write_file_without_comments(in, out);
-
 }
 
 /*Write file without comments */
 FILE *write_file_without_comments(FILE *in, FILE *out) {
     int ch, state;
     state = OUT;
+    /*Reading and writing to file*/
     while ((ch = fgetc(in)) != EOF) {
         switch (state) {
             case OUT:
-                if (ch == '/') {
-                    state = LEFT_SLASH;
-                } else {
-                    putc(ch, out);
-                    if (ch == '\"') {
-                        state = IN_STRING;
-                    }
-                }
+                state = check_out_state_change(out, ch, state);
                 break;
-
             case LEFT_SLASH:
-                if (ch == '*') {
-                    state = IN_COMMENT;
-                }
-                break;
-
             case IN_COMMENT:
-                if (ch == '*') {
-                    state = RIGHT_STAR;
-
-                }
+                state = check_star_state(ch, state);
                 break;
-
             case RIGHT_STAR:
-                if (ch == '/') {
-                    state = OUT;
-                } else {
-                    state = IN_COMMENT;
-                }
+                state = check_slash_state(ch, state);
                 break;
             case IN_STRING:
-                if (ch == '\"') {
-                    state = OUT;
-                }
+                state = check_string_state(ch, state);
                 putc(ch, out);
                 break;
         }
-        /*todo del*/
-        printf("%c:%d", ch, ch);
     }
     return out;
 }
 
+/*Check if write normally or change state from OUT*/
+int check_out_state_change(FILE *out, int ch, int state) {
+    if (IS_SLASH) {
+        return LEFT_SLASH;
+    } else {
+        putc(ch, out);
+        return check_string_state(ch, state);
+    }
+}
+
+/*Check state change on * value*/
+int check_star_state(int ch, int state) {
+    if (IS_STAR) {
+        return (state == LEFT_SLASH) ? IN_COMMENT : RIGHT_STAR;
+    }
+    return state;
+}
+
+int check_slash_state(int ch, int state) {
+    if (IS_SLASH && state != IN_COMMENT) {
+        return (state == OUT) ? LEFT_SLASH : OUT;
+    }
+    return state;
+}
+
+/*Check if keep IN_STRING state*/
+int check_string_state(int ch, int state) {
+    if (IS_QUOTE) {
+        return (state == IN_STRING) ? OUT : IN_STRING;
+    }
+    return state;
+}
--- comment_remover.h
+++ comment_remover.h
@@ -2,9 +2,25 @@
 #include "file_setup_util.h"
 
 /*Definitions*/
-#define MAX_BUFFER 500
-
 #define READ_MODE "r"
 
+#define WRITE_MODE "w"
+
+#define IS_SLASH ch == '/'
+
+#define IS_STAR ch == '*'
+
+#define IS_QUOTE ch=='\"'
+
 /*Functions*/
-FILE * generate_comment_free_file(char *fileName);
+FILE *generate_comment_free_file(char *fileName);
+
+FILE *write_file_without_comments(FILE *in, FILE *out);
+
+int check_out_state_change(FILE *out, int ch, int state);
+
+int check_string_state(int ch, int state);
+
+int check_star_state(int ch, int state);
+
+int check_slash_state(int ch, int state);
