bba37bdd2c9caa3df717e93d913bcd23f756b8c9
george <George.mpro87@gmail.com>
2021-01-12 20:33:39 +0200

    All works

---
+++ input_test_1.c
@@ -0,0 +1,10 @@
+     /*comment*/   #include "header1.h"/*comment*/
+#       include           /*comment*/      "header2.h"
+
+int func(){
+
+    printf("Hello World!"/*comment*/);
+    printf("include");
+
+    return 0;
+}
